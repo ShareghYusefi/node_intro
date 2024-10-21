69e5e4d3a385bb902bd7aad1ad60b4772ce8a8e6
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-10-21 17:52:42 -0600

    built in modules

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 // http module is a built-in module in Node.js
 // we can use it to create a web server - a server that listens for requests and sends responses to the client
 
+// any file in Node.js is considered a module, giving us the ability to export and import code between files/modules
+// console.log(module);
+
+// What are some of the built-in modules in Node.js?
+// http for creating web servers
+
+// fs for reading and writing files
+// we can create, read, update, and delete files using the fs module
+
+// path for working with file paths
+const path = require("path");
+const x = path.parse(__filename); // __filename is a global object that gives us the path to the current file
+console.log(x);
+console.log(__filename);
+
+// os for getting information about the operating system
+const os = require("node:os");
+var type = os.type();
+var freeMemory = os.freemem();
+var totalMemory = os.totalmem();
+console.log(`Operating System: ${type}`);
+console.log(`Free Memory: ${freeMemory}`);
+console.log(`Total Memory: ${totalMemory}`);
+
 // We can use the require() function to include a module in our project
 var http = require("http");
 
